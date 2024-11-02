8bd48407009d5cae2d6e8c6ac63dce3a6e04e416
Kayla Lei <kaylalei@macbook-pro-538.wifi.local.cmu.edu>
2024-11-02 19:08:35 -0400

    web app db access and initial frontend ui

---
+++ client/src/pages/components/MainContent.js
@@ -0,0 +1,297 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Chip from '@mui/material/Chip';
+import Grid from '@mui/material/Grid2';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import { styled } from '@mui/material/styles';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import RssFeedRoundedIcon from '@mui/icons-material/RssFeedRounded';
+import { Star, StarHalf, StarBorder } from '@mui/icons-material';
+
+const cardData = [
+  {
+    img: 'https://www.allrecipes.com/thmb/GvI7NxsOoNqbP52MM8bsaTWFUSE=/0x512/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/76594-grandmothers-buttermilk-cornbread-beauty-3x4-0135064414801604-dd84dcaf1d0f421eb3b6fdced8cf0ad3.jpg',
+    tag: 'Dessert',
+    title: "Grandmother's Buttermilk Cornbread",
+    description:
+      "The best cornbread recipe is my grandmother's, and this is it — sweet and moist!",
+    stars: 4.8,
+    ratings: 7508
+  },
+  {
+    img: 'https://www.allrecipes.com/thmb/iOfxQGOJTdM0K6edW-MFkn-nydE=/0x512/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/23600-worlds-best-lasagna-DDMFS-4x3-1196-24c5401652934ffb96d3d94bc9fbe2d7.jpg',
+    tag: 'Dinner',
+    title: "World's Best Lasagna",
+    description:
+      "This lasagna recipe takes a little work, but it is so satisfying and filling that it's worth it!",
+    stars: 4.8,
+    ratings: 20691
+  },
+];
+
+const SyledCard = styled(Card)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: 0,
+  height: '100%',
+  backgroundColor: (theme.vars || theme).palette.background.paper,
+  '&:hover': {
+    backgroundColor: 'transparent',
+    cursor: 'pointer',
+  },
+  '&:focus-visible': {
+    outline: '3px solid',
+    outlineColor: 'hsla(210, 98%, 48%, 0.5)',
+    outlineOffset: '2px',
+  },
+}));
+
+const SyledCardContent = styled(CardContent)({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 4,
+  padding: 16,
+  flexGrow: 1,
+  '&:last-child': {
+    paddingBottom: 16,
+  },
+});
+
+const StyledTypography = styled(Typography)({
+  display: '-webkit-box',
+  WebkitBoxOrient: 'vertical',
+  WebkitLineClamp: 2,
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+});
+
+function getStarComponent(starType) {
+  console.log(starType)
+  if (starType === "empty") return <StarBorder/>;
+  if (starType === "half") return <StarHalf/>;
+  return <Star/>;
+}
+
+function Stars({ starCount, ratings }) {
+  let stars = starCount
+  let starIcons = [];
+  while (starCount > 0) {
+    console.log(starCount)
+    if (starCount <= 0) starIcons.push("empty");
+    else if (starCount < 0.5) starIcons.push("half");
+    else starIcons.push("full");
+    starCount -= 1;
+  }
+  console.log(starIcons)
+
+  return (
+    <span>
+      {starIcons.map((starType) => (
+        getStarComponent(starType)
+      ))}
+      <Typography>{`${stars} stars from ${ratings} ratings`}</Typography>
+    </span>
+  )
+}
+
+export function Search() {
+  return (
+    <FormControl sx={{ width: { xs: '100%', md: '25ch' } }} variant="outlined">
+      <OutlinedInput
+        size="small"
+        id="search"
+        placeholder="Search recipes"
+        sx={{ flexGrow: 1 }}
+        startAdornment={
+          <InputAdornment position="start" sx={{ color: 'text.primary' }}>
+            <SearchRoundedIcon fontSize="small" />
+          </InputAdornment>
+        }
+        inputProps={{
+          'aria-label': 'search',
+        }}
+      />
+    </FormControl>
+  );
+}
+
+export default function MainContent() {
+  const [focusedCardIndex, setFocusedCardIndex] = React.useState(null);
+
+  const handleFocus = (index) => {
+    setFocusedCardIndex(index);
+  };
+
+  const handleBlur = () => {
+    setFocusedCardIndex(null);
+  };
+
+  const handleClick = () => {
+    console.info('You clicked the filter chip.');
+  };
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+      <Box
+        sx={{
+          display: { xs: 'flex', sm: 'none' },
+          flexDirection: 'row',
+          gap: 1,
+          width: { xs: '100%', md: 'fit-content' },
+          overflow: 'auto',
+        }}
+      >
+        <Search />
+        <IconButton size="small" aria-label="RSS feed">
+          <RssFeedRoundedIcon />
+        </IconButton>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column-reverse', md: 'row' },
+          width: '100%',
+          justifyContent: 'space-between',
+          alignItems: { xs: 'start', md: 'center' },
+          gap: 4,
+          overflow: 'auto',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'inline-flex',
+            flexDirection: 'row',
+            gap: 3,
+            overflow: 'auto',
+          }}
+        >
+          <Chip onClick={handleClick} size="medium" label="All categories" />
+          <Chip
+            onClick={handleClick}
+            size="medium"
+            label="Breakfast"
+            sx={{
+              backgroundColor: 'transparent',
+              border: 'none',
+            }}
+          />
+          <Chip
+            onClick={handleClick}
+            size="medium"
+            label="Lunch"
+            sx={{
+              backgroundColor: 'transparent',
+              border: 'none',
+            }}
+          />
+          <Chip
+            onClick={handleClick}
+            size="medium"
+            label="Dinner"
+            sx={{
+              backgroundColor: 'transparent',
+              border: 'none',
+            }}
+          />
+          <Chip
+            onClick={handleClick}
+            size="medium"
+            label="Dessert"
+            sx={{
+              backgroundColor: 'transparent',
+              border: 'none',
+            }}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: { xs: 'none', sm: 'flex' },
+            flexDirection: 'row',
+            gap: 1,
+            width: { xs: '100%', md: 'fit-content' },
+            overflow: 'auto',
+          }}
+        >
+          <Search />
+          <IconButton size="small" aria-label="RSS feed">
+            <RssFeedRoundedIcon />
+          </IconButton>
+        </Box>
+      </Box>
+      <Grid container spacing={2} columns={12}>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <SyledCard
+            variant="outlined"
+            onFocus={() => handleFocus(0)}
+            onBlur={handleBlur}
+            tabIndex={0}
+            className={focusedCardIndex === 0 ? 'Mui-focused' : ''}
+          >
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              image={cardData[0].img}
+              sx={{
+                aspectRatio: '16 / 9',
+                borderBottom: '1px solid',
+                borderColor: 'divider',
+              }}
+            />
+            <SyledCardContent>
+              <Typography gutterBottom variant="caption" component="div">
+                {cardData[0].tag}
+              </Typography>
+              <Typography gutterBottom variant="h6" component="div">
+                {cardData[0].title}
+              </Typography>
+              <StyledTypography variant="body2" color="text.secondary" gutterBottom>
+                {cardData[0].description}
+              </StyledTypography>
+              <Stars starCount={cardData[0].stars} ratings={cardData[0].ratings} />
+            </SyledCardContent>
+            
+          </SyledCard>
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <SyledCard
+            variant="outlined"
+            onFocus={() => handleFocus(1)}
+            onBlur={handleBlur}
+            tabIndex={0}
+            className={focusedCardIndex === 1 ? 'Mui-focused' : ''}
+          >
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              image={cardData[1].img}
+              aspect-ratio="16 / 9"
+              sx={{
+                borderBottom: '1px solid',
+                borderColor: 'divider',
+              }}
+            />
+            <SyledCardContent>
+              <Typography gutterBottom variant="caption" component="div">
+                {cardData[1].tag}
+              </Typography>
+              <Typography gutterBottom variant="h6" component="div">
+                {cardData[1].title}
+              </Typography>
+              <StyledTypography variant="body2" color="text.secondary" gutterBottom>
+                {cardData[1].description}
+              </StyledTypography>
+              <Stars starCount={cardData[1].stars} ratings={cardData[1].ratings} />
+            </SyledCardContent>
+          </SyledCard>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const axios = require("axios");
 const { JSDOM } = require('jsdom');
+const { Client } = require('pg');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -14,13 +15,17 @@ app.listen(8080, () => {
 
 const client = new Client({
     user: 'klei',
-    password: 'Man-45663',
-    host: 'database.server.com',
+    password: 'hey-chef-db-password',
+    host: 'hey-chef-db.cngq6g8kks7t.us-east-1.rds.amazonaws.com',
     port: 5432,
     database: 'mydb',
+    ssl: {
+        require: true,
+        rejectUnauthorized: false,  // Bypass certificate verification if you don't have a local cert
+      }
 })
-   
-await client.connect()
+console.log('created Client object');
+
 
 const createTableQueryText = `
     CREATE TABLE IF NOT EXISTS breakfast(
@@ -29,20 +34,114 @@ const createTableQueryText = `
     time TEXT,
     servings TEXT,
     rating TEXT,
-    starCount SMALLINT,
+
     steps TEXT []
     );`;
 
-try {
-    await client.query(queryText);
-    console.log('Table created successfully');
-} catch (error) {
-    console.error('Error creating table:', error);
-}
+// try {
+//     await client.query(queryText);
+//     console.log('Table created successfully');
+// } catch (error) {
+//     console.error('Error creating table:', error);
+// }
 
 
+const breakfastUrls = ['https://www.allrecipes.com/recipe/21014/good-old-fashioned-pancakes/',
+                       'https://www.allrecipes.com/recipe/213717/chakchouka-shakshouka/',
+                       'https://www.allrecipes.com/recipe/16895/fluffy-french-toast/',
+                       'https://www.allrecipes.com/recipe/244251/no-cook-overnight-oatmeal/',
+                       'https://www.allrecipes.com/recipe/15057/overnight-blueberry-french-toast/',
+                       'https://www.allrecipes.com/recipe/220895/old-charleston-style-shrimp-and-grits/',
+                       'https://www.allrecipes.com/recipe/162760/fluffy-pancakes/',
+                       'https://www.allrecipes.com/recipe/257657/lemon-ricotta-pancakes/',
+                       'https://www.allrecipes.com/recipe/87013/hash-brown-and-egg-casserole/',
+                       'https://www.allrecipes.com/recipe/24148/easy-broccoli-quiche/',
+                       'https://www.allrecipes.com/recipe/16383/basic-crepes/',
+                       'https://www.allrecipes.com/recipe/51013/baked-oatmeal-ii/',
+                       'https://www.allrecipes.com/recipe/98390/megans-granola/',
+                       'https://www.allrecipes.com/recipe/17205/eggs-benedict/',
+                       'https://www.allrecipes.com/recipe/20513/classic-waffles/',
+                       'https://www.allrecipes.com/recipe/279754/chef-johns-blueberry-dutch-baby/',
+                       'https://www.allrecipes.com/recipe/20334/banana-pancakes-i/',
+                       'https://www.allrecipes.com/recipe/23900/german-apple-pancake/',
+                       'https://www.allrecipes.com/recipe/244767/cinnamon-roll-pancakes/',
+                       'https://www.allrecipes.com/recipe/228426/loaded-vegetarian-quiche/',
+                       'https://www.allrecipes.com/recipe/24532/sausage-casserole/',
+                       'https://www.allrecipes.com/recipe/143886/creamy-strawberry-crepes/',
+                       'https://www.allrecipes.com/recipe/57783/emilys-famous-hash-browns/'
+                    ];
 
+const lunchUrls = ['https://www.allrecipes.com/recipe/23891/grilled-cheese-sandwich/',
+                   'https://www.allrecipes.com/recipe/187342/banh-mi/',
+                   'https://www.allrecipes.com/recipe/47717/reuben-sandwich-ii/',
+                   'https://www.allrecipes.com/recipe/188473/buffalo-chicken-wraps/',
+                   'https://www.allrecipes.com/recipe/13309/rich-and-simple-french-onion-soup/',
+                   'https://www.allrecipes.com/recipe/77981/butternut-squash-soup-ii/',
+                   'https://www.allrecipes.com/recipe/26460/quick-and-easy-chicken-noodle-soup/',
+                   'https://www.allrecipes.com/recipe/13113/rich-and-creamy-tomato-basil-soup/',
+                   'https://www.allrecipes.com/recipe/39544/garden-fresh-tomato-soup/',
+                   'https://www.allrecipes.com/recipe/49552/quinoa-and-black-beans/',
+                   'https://www.allrecipes.com/recipe/62763/terrific-turkey-chili/',
+                   'https://www.allrecipes.com/recipe/20447/cheesy-tuna-melts/',
+                   'https://www.allrecipes.com/recipe/85933/the-ultimate-pasta-salad/',
+                   'https://www.allrecipes.com/recipe/143069/super-delicious-zuppa-toscana/',
+                   'https://www.allrecipes.com/recipe/13045/broccoli-cheese-soup/',
+                   'https://www.allrecipes.com/recipe/73963/pasta-salad-with-homemade-dressing/',
+                   'https://www.allrecipes.com/recipe/13933/black-bean-and-corn-salad-ii/',
+                   'https://www.allrecipes.com/recipe/24264/sloppy-joes-ii/',
+                   'https://www.allrecipes.com/recipe/47717/reuben-sandwich-ii/',
+                   'https://www.allrecipes.com/recipe/147103/delicious-egg-salad-for-sandwiches/',
+                   'https://www.allrecipes.com/recipe/14415/cobb-salad/',
+                   'https://www.allrecipes.com/recipe/16729/old-fashioned-potato-salad/'
+                ];
 
+const dinnerUrls = ['https://www.allrecipes.com/recipe/23600/worlds-best-lasagna/',
+                    'https://www.allrecipes.com/recipe/158140/spaghetti-sauce-with-ground-beef/',
+                    'https://www.allrecipes.com/recipe/172958/no-noodle-zucchini-lasagna/',
+                    'https://www.allrecipes.com/recipe/14685/slow-cooker-beef-stew-i/',
+                    'https://www.allrecipes.com/recipe/15925/creamy-au-gratin-potatoes/',
+                    'https://www.allrecipes.com/recipe/26317/chicken-pot-pie-ix/',
+                    'https://www.allrecipes.com/recipe/16354/easy-meatloaf/',
+                    'https://www.allrecipes.com/recipe/216888/good-new-orleans-creole-gumbo/',
+                    'https://www.allrecipes.com/recipe/31848/jambalaya/',
+                    'https://www.allrecipes.com/recipe/11758/baked-ziti-i/',
+                    'https://www.allrecipes.com/recipe/11679/homemade-mac-and-cheese/',
+                    'https://www.allrecipes.com/recipe/18379/best-green-bean-casserole/',
+                    'https://www.allrecipes.com/recipe/242352/greek-lemon-chicken-and-potatoes/',
+                    'https://www.allrecipes.com/recipe/12720/grilled-salmon-i/',
+                    'https://www.allrecipes.com/recipe/14759/pork-dumplings/',
+                    'https://www.allrecipes.com/recipe/17991/stuffed-green-peppers-i/',
+                    'https://www.allrecipes.com/recipe/18417/spanakopita-greek-spinach-pie/',
+                    'https://www.allrecipes.com/recipe/14497/portobello-mushroom-burgers/',
+                    'https://www.allrecipes.com/recipe/58211/authentic-louisiana-red-beans-and-rice/',
+                    'https://www.allrecipes.com/recipe/79543/fried-rice-restaurant-style/',
+                    'https://www.allrecipes.com/recipe/85389/gourmet-mushroom-risotto/',
+                    'https://www.allrecipes.com/recipe/228823/quick-beef-stir-fry/'
+                ];
+
+const dessertUrls = ['https://www.allrecipes.com/recipe/12409/apple-crisp-ii/',
+                     'https://www.allrecipes.com/recipe/52547/triple-berry-crisp/',
+                     'https://www.allrecipes.com/recipe/8350/chantals-new-york-cheesecake/',
+                     'https://www.allrecipes.com/recipe/13477/double-layer-pumpkin-cheesecake/',
+                     'https://www.allrecipes.com/recipe/169305/sopapilla-cheesecake-pie/',
+                     'https://www.allrecipes.com/recipe/9174/peanut-butter-pie/',
+                     'https://www.allrecipes.com/recipe/12682/apple-pie-by-grandma-ople/',
+                     'https://www.allrecipes.com/recipe/12142/sweet-potato-pie-i/',
+                     'https://www.allrecipes.com/recipe/15093/grandmas-lemon-meringue-pie/',
+                     'https://www.allrecipes.com/recipe/23439/perfect-pumpkin-pie/',
+                     'https://www.allrecipes.com/recipe/17377/chocolate-cupcakes/',
+                     'https://www.allrecipes.com/recipe/153245/pumpkin-spice-cupcakes/',
+                     'https://www.allrecipes.com/recipe/230204/maris-banana-cupcakes/',
+                     'https://www.allrecipes.com/recipe/79313/carrot-cupcakes-with-white-chocolate-cream-cheese-icing/',
+                     'https://www.allrecipes.com/recipe/22749/the-best-banana-pudding/',
+                     'https://www.allrecipes.com/recipe/7177/bread-pudding-ii/',
+                     'https://www.allrecipes.com/recipe/19165/creme-brulee/',
+                     'https://www.allrecipes.com/limoncello-ricotta-cake-recipe-7970804',
+                     'https://www.allrecipes.com/recipe/25037/best-big-fat-chewy-chocolate-chip-cookie/',
+                     'https://www.allrecipes.com/recipe/17891/golden-sweet-cornbread/',
+                     'https://www.allrecipes.com/recipe/17981/one-bowl-chocolate-cake-iii/',
+                     'https://www.allrecipes.com/recipe/9827/chocolate-chocolate-chip-cookies-i/'
+                ];
 
 const websites = ['https://www.allrecipes.com/recipe/158140/spaghetti-sauce-with-ground-beef/',
                   'https://www.allrecipes.com/recipe/17891/golden-sweet-cornbread/',
@@ -65,6 +164,13 @@ recipe {
 
 async function getRecipePage() {
 	try {
+        // console.log('in getRecipePage');
+        // await client.connect();
+        // console.log('connected');
+        // await client.query(createTableQueryText);
+        // console.log("table created successfully");
+
+
 		// Request recipe and "await" the response
 
         for (let j = 0; j < websites.length; j++) {
@@ -113,30 +219,37 @@ async function getRecipePage() {
             // steps
             const stepElements = dom.window.document.getElementById("mm-recipes-steps__content_1-0")
                                                     .getElementsByClassName("comp mntl-sc-block mntl-sc-block-html");
-            const steps = []
+            let steps = []
             for (let i = 0; i < stepElements.length; i++) {
-                steps.push(stepElements[i].textContent.trim());
+                let splitByPeriod = stepElements[i].textContent.trim().split(". ");
+                steps = [...steps, ...splitByPeriod];
             }
+            console.log(steps)
 
             let recipeData = {
                 title: recipeTitle,
-                // ingredients: ingredients,
+                ingredientsJson: ingredientsJson,
+                ingredientsMap: ingredientsMap,
                 time: time,
                 servings: servings,
                 rating: rating,
-                starCount: Number(starCount),
+                // starCount: Number(starCount),
                 steps: steps,
                 // htmlFile: htmlFile
 
             }
 
-            let insertQueryText = `
-                INSERT INTO breakfast(title, ingredients, time, servings, rating, starCount, steps)
-                VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+            // let insertQueryText = `
+            //     INSERT INTO breakfast(title, ingredients, time, servings, rating, steps)
+            //     VALUES ($1, $2, $3, $4, $5, $6)`;
 
-            client.query(insertQueryText, [recipeTitle, ingredientsJson, time, servings, rating, starCount, steps]);
+            // client.query(insertQueryText, [recipeTitle, ingredientsJson, time, servings, rating, steps]);
 
-    
+            console.log(recipeData);
+
+            // const res = await client.query('SELECT * FROM breakfast');
+            // console.log(res)
+            // await client.end();
         }
 
 	} catch (error) {
